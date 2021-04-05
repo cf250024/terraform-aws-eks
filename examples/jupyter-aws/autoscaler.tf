c2cd3f1e0276a0464d0eab9473a2220b31f44ffd
cf250024 <chengjun.fu@teradata.com>
2021-04-05 13:42:36 -0700

    fix typo

--- examples/jupyter-aws/autoscaler.tf
+++ examples/jupyter-aws/autoscaler.tf
@@ -1,6 +1,4 @@
-terresource "" "name" {
-  
-}# Create IAM role + automatically make it available to cluster autoscaler service account
+# Create IAM role + automatically make it available to cluster autoscaler service account
 module "iam_assumable_role_admin" {
   source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version                       = "~> v3.13.0"
