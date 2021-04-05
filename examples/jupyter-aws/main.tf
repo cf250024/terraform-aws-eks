979a1d4c308257e79686a769a3abaf2fcea90b0c
cf250024 <chengjun.fu@teradata.com>
2021-04-05 13:25:22 -0700

    update version

--- examples/jupyter-aws/main.tf
+++ examples/jupyter-aws/main.tf
@@ -8,22 +8,6 @@ provider "aws" {
   profile = var.profile
 }
 
-provider "template" {
-  version = "~> 2.1"
-}
-
-provider "random" {
-  version = "~> 2.1"
-}
-
-provider "local" {
-  version = "~> 1.4"
-}
-
-provider "null" {
-  version = "~> 2.1"
-}
-
 data "aws_caller_identity" "current" {}
 
 data "aws_eks_cluster" "cluster" {
@@ -39,7 +23,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
   load_config_file       = false
-  version                = "~> 1.11"
+  version                = "~> 1.13.3"
 }
 
 data "aws_availability_zones" "available" {
@@ -78,7 +62,6 @@ module "vpc" {
 }
 
 module "eks" {
-  # version = "~> 13.0.0"
   source          = "../.."
   cluster_name    = var.cluster_name
   cluster_version = "1.18"
